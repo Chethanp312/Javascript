edded55d4078cd0008794fdff2daf40fb444853c
Chethanp312 <57157929+Chethanp312@users.noreply.github.com>
2023-09-25 21:23:18 +0000

    datatype-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100; // JS is dynamic in assigning datatypes
+
+const id = Symbol('123');
+
+
+
+// Reference type or Non-primitive
+// Array, objects, Functions
+
+const heros =["shaktiman", "Naagraj", "doga"];
+
+let myObj={
+    name: "Chethan",
+    age: 35
+}
+
+//function(){}

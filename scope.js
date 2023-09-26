b2d6e4ffa84504ae7f8d32124c955b241d94f921
Chethanp312 <57157929+Chethanp312@users.noreply.github.com>
2023-09-26 20:55:45 +0000

    scope

---
+++ scope.js
@@ -0,0 +1,21 @@
+let a=10;
+const b=20;
+var c= 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+if(true){
+
+}
+
+
+function one(){
+    const username = "chethan";
+
+    function two(){
+        const website ="youtube";
+    }
+}
+
